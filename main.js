ccdc585cbdcd1110d74e4f8d3fd24f2d7fe1a83d
tmrk <tamas.marki@gmail.com>
2018-04-23 10:05:00 +1300

    c4

--- main.js
+++ main.js
@@ -30,74 +30,116 @@ fetch('lexin.json').then(function(response) {
 }).then(function(words) {
     words = words.words;
 
-    function getArticle(query) {
-        var wordList = words.filter(function(el) {
-            return el.form.toLowerCase().replace(/[0-9,~]/g, '').startsWith(query.toLowerCase());
-        });
-        return {
-            wordList: wordList
+    function findWord(word) {
+        var wordsToShow = words.filter(function(el) {
+                return el.form.toLowerCase().replace(/[0-9,~]/g, '').trim() == word;
+            });
+        if (wordsToShow[0] && wordsToShow[0].form.slice(-1) == '1') for (var i = 1; i < wordsToShow.length; i++) {
+            if (isNaN(wordsToShow[i].form.slice(-1)) || wordsToShow[i].form.slice(-1) == '1') break;
+        }
+        return wordsToShow;
+    }
+
+    function showWord(wordsToShow) {
+        if (wordsToShow) {
+            document.getElementById('result').innerHTML = '';
+            for (var i = 0; i < wordsToShow.length; i++) {
+                var word = wordsToShow[i],
+                    formattedWord = (word.form && isNaN(word.form.slice(-1))) ?
+                    t('h1', word.form) : word.form ?
+                    t('h1', [
+                        word.form.slice(0, -2),
+                        t('sup', word.form.slice(-1))
+                    ]) : '',
+                    pronunciation = word.pronunciation ? t('span.pronunciation', ' | ' + word.pronunciation + ' |') : '',
+                    pos = word.pos ? t('span.pos', word.pos) : '',
+                    inflection = word.inflection ? t('span.inflection', ' (' + word.inflection + ')') : '',
+                    lexeme = t('div.lexeme');
+                if (word.lexeme) {
+                    for (var ii = 0; ii < word.lexeme.length; ii++) {
+                        var lex = word.lexeme[ii] ? word.lexeme[ii] : {},
+                            definition = lex.definition ? t('span.definition', lex.definition) : '',
+                            definition_comm = lex.definition_comm ? t('span.definition_comm', lex.definition_comm) : '',
+                            valency = lex.valency ? t('span.valency', lex.valency) : '',
+                            grammat_comm = lex.grammat_comm ? t('span.grammat_comm', lex.grammat_comm) : '',
+                            example = lex.example ? t('span.example', lex.example.join(', ')) : '',
+                            usage = lex.usage ? t('span.usage', lex.usage) : '';
+
+                        lexeme.appendChild(t('p', [definition, valency, grammat_comm, example, usage]));
+                    }
+                }
+                result.appendChild(t('div.word', [formattedWord, pronunciation, t('br'), pos, inflection, lexeme]));
+            }
         }
     }
 
     document.body.appendChild(
         t('div#wrapper', [
             t('input#word|placeholder=Type a word to look up', '', {'input': function() {
-                var main = document.getElementById('main');
-                main.innerHTML = '';
+                var main = document.getElementById('main')
+                    filtered = document.getElementById('filtered'),
+                    result = document.getElementById('result');
+                filtered.innerHTML = '';
                 if (this.value) {
                     document.getElementById('wrapper').classList.add('on');
-                    var search = getArticle(this.value);
 
-                    var filtered = t('ul#filtered'),
-                        result = t('div#result');
-
-                    var wordList = search.wordList;
-
-                    main.appendChild(filtered);
-                    main.appendChild(result);
-
+                    var wordList = words.filter(function(el) {
+                        return el.form.toLowerCase().replace(/[0-9,~]/g, '').startsWith(this.value.toLowerCase());
+                    }, (this));
                     for (var i = 0; i < wordList.length; i++) {
                         if (isNaN(wordList[i].form.slice(-1))) {
-                            filtered.appendChild(t('li', wordList[i].form.replace('~','')));
+                            filtered.appendChild(t('li', wordList[i].form.replace('~',''), {'click': function() {
+                                document.getElementsByClassName('selected')[0].classList.remove('selected');
+                                this.classList.add('selected');
+                                showWord(findWord(this.innerHTML));
+                            }}));
                         } else if (wordList[i].form.slice(-1) == '1') {
-                            filtered.appendChild(t('li', wordList[i].form.slice(0, -2).replace('~','')));
+                            filtered.appendChild(t('li', wordList[i].form.slice(0, -2).replace('~',''), {'click': function() {
+                                document.getElementsByClassName('selected')[0].classList.remove('selected');
+                                this.classList.add('selected');
+                                showWord(findWord(this.innerHTML));
+                            }}));
                         }
                     }
-                    function showWord(word) {
-                        if (word) {
-                            var formattedWord = (word.form && isNaN(word.form.slice(-1))) ?
-                                t('h1', word.form) : word.form ?
-                                t('h1', [
-                                    word.form.slice(0, -2),
-                                    t('sup', word.form.slice(-1))
-                                ]) : '',
-                                pronunciation = word.pronunciation ? t('span.pronunciation', ' | ' + word.pronunciation + ' |') : '',
-                                pos = word.pos ? t('span.pos', word.pos) : '',
-                                inflection = word.inflection ? t('span.inflection', ' (' + word.inflection + ')') : '',
-                                lexeme = t('div.lexeme');
-
-                            for (var i = 0; i < word.lexeme.length; i++) {
-                                var definition = word.lexeme[i].definition ? t('span.definition', word.lexeme[i].definition) : '',
-                                    definition_comm = word.lexeme[i].definition_comm ? t('span.definition_comm', word.lexeme[i].definition_comm) : '',
-                                    valency = word.lexeme[i].valency ? t('span.valency', word.lexeme[i].valency) : '',
-                                    grammat_comm = word.lexeme[i].grammat_comm ? t('span.grammat_comm', word.lexeme[i].grammat_comm) : '',
-                                    example = word.lexeme[i].example ? t('span.example', word.lexeme[i].example.join(', ')) : '',
-                                    usage = word.lexeme[i].usage ? t('span.usage', word.lexeme[i].usage) : '';
-
-                                lexeme.appendChild(t('p', [definition, valency, grammat_comm, example, usage]));
-                            }
-                            result.appendChild(t('div.word', [formattedWord, pronunciation, t('br'), pos, inflection, lexeme]));
-                        }
-                    }
-                    showWord(wordList[0]);
+                    filtered.firstChild.classList.add('selected');
+                    var wordsToShow = [wordList[0]];
                     if (wordList[0] && wordList[0].form.slice(-1) == '1') for (var i = 1; i < wordList.length; i++) {
                         if (isNaN(wordList[i].form.slice(-1)) || wordList[i].form.slice(-1) == '1') break;
-                        showWord(wordList[i]);
+                        wordsToShow.push(wordList[i]);
                     }
+                    showWord(wordsToShow);
                 } else document.getElementById('wrapper').classList.remove('on');
             }}),
-            t('div#main')
+            t('div#main', [
+                t('ul#filtered'),
+                t('div#result')
+            ])
         ])
     );
-    document.getElementById('word').focus();
+    document.addEventListener('keydown', function(e) {
+        var selected = document.getElementsByClassName('selected')[0];
+        switch (e.keyCode) {
+            case 40:
+                e.preventDefault();
+                //down();
+                if (selected.nextSibling) {
+                    selected.classList.remove('selected');
+                    selected.nextSibling.classList.add('selected');
+                    showWord(findWord(selected.nextSibling.innerHTML));
+                }
+                break;
+            case 38:
+                e.preventDefault();
+                //up();
+                if (selected.previousSibling) {
+                    selected.classList.remove('selected');
+                    selected.previousSibling.classList.add('selected');
+                    showWord(findWord(selected.previousSibling.innerHTML));
+                }
+                break;
+        }
+    });
+    document.addEventListener('DOMContentLoaded', function() {
+        document.getElementById('word').focus();
+    }, false);
 });
