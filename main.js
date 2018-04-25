bbaa261b22c1053cda6919d6dcef29d5cfec1ca8
tmrk <tamas.marki@gmail.com>
2018-04-25 14:30:09 +1300

    14

--- main.js
+++ main.js
@@ -196,4 +196,6 @@ fetch('lexin.json').then(function(response) {
                 break;
         }
     })
+}).then(function() {
+    document.body.removeChild(document.getElementById('loading'));
 })
